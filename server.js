59afc7cfc4e95428c65eecc4eb4f92de37ca10ed
PH <oster.inbox@gmail.com>
2026-05-26 14:38:40 -0400

    fix: use pg adapter for Prisma 7, add dev script, remove dotenv

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const path = require('path');
 const twilio = require('twilio');
+const { Pool } = require('pg');
+const { PrismaPg } = require('@prisma/adapter-pg');
 const { PrismaClient } = require('@prisma/client');
 
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
+
 const app = express();
-const prisma = new PrismaClient();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
